6dd3019a3369d1f5cef0943f90bc6c619cd53cd0
lesteven <stevenle2011@gmail.com>
2019-08-12 18:01:11 -0700

    feat: add new components

---
+++ src/client/comp/ErrorPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const ErrorPage = () => {
+  return (
+    <p> This page is not available </p>
+  )
+}
+
+export default ErrorPage;
--- src/client/comp/NavBar.jsx
+++ src/client/comp/NavBar.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../routes';
 import css from './css/nav.css';
+import { getData } from '../custHooks/getData';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginAC } from '../redux/user';
 
 const NavBar = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const get = async() => {
+      const json = await getData('http://localhost:3030/api/sessions');
+      dispatch(loginAC(json));
+    }
+    get();
+  }, [])
+
+  const user = useSelector(state => state.user);
+
+  let filteredRoutes = routes;
+  if (user.username != '') {
+    filteredRoutes = routes.filter(e => {
+      return e.path != '/signup' && e.path != '/login'
+    });
+    filteredRoutes = filteredRoutes.slice(0, filteredRoutes.length-1);
+  } else {
+    filteredRoutes = routes.filter(e => e.path != '/prof');
+  }
+
   return (
     <nav className = 'nav-bar'>
-      {routes.map(e => 
+      {filteredRoutes.map(e => 
         <Link to = {e.path} key = {e.path}> {e.title} </Link>
       )}
+      { user.username != '' ? 
+          <Link to = {`/prof/${user.username}`} > Profile </Link> : null }
     </nav>
   )
 }
